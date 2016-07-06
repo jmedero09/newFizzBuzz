9a982b7cc1f12767756dd85d1e1cfc14a8747869
Jesus Medero <jmedero09@gmail.com>
2016-07-06 06:55:29 -0400

    inital commit

---
+++ js/app.js
@@ -0,0 +1,60 @@
+var x = parseInt(prompt("Put in a number"));
+document.write(x);
+function fizz(number){
+	
+
+	for(var i=1;i<=number;i++){
+		if((i%3===0)&&(i%5===0)){
+			document.write("fizzBuzz"+"<br>");
+		}
+		else if(i%3===0){
+			document.write("fizz"+"<br>");
+		}
+		else if(i%5===0){
+			document.write("buzz"+"<br>");
+		}
+		else{
+			document.write(i+"<br>");
+		}		
+	}
+}
+
+
+fizz(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
